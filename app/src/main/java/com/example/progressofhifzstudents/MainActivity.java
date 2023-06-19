5c06fad1a6bdbb88017c7080eeba7a41d4d4f16d
Azib Muzammil <115615306+azibmuzammil@users.noreply.github.com>
2023-06-19 07:37:16 -0700

    Functionality of Add student Button

--- app/src/main/java/com/example/progressofhifzstudents/MainActivity.java
+++ app/src/main/java/com/example/progressofhifzstudents/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.progressofhifzstudents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button addStudentButton = findViewById(R.id.addStudent);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button addStudentRecordButton = findViewById(R.id.addStudentRecord);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button searchStudentButton = findViewById(R.id.searchStudent);
+        Button gitHubButton = findViewById(R.id.gitHub);
+
+        addStudentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, addStudents.class));
+            }
+        });
+
+
+
+     
     }
 }
