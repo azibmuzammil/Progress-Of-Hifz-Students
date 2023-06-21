0beb710221cdb88065707442ae5010a22af3ee11
Azib Muzammil <115615306+azibmuzammil@users.noreply.github.com>
2023-06-21 14:21:36 -0700

    Getter Setter for Student credentials

---
+++ app/src/main/java/com/example/progressofhifzstudents/Student.java
@@ -0,0 +1,47 @@
+package com.example.progressofhifzstudents;
+
+public class Student {
+    private String id;
+    private String name;
+    private String age;
+    private String className;
+
+    public Student(String id, String name, String age, String className) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.className = className;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
